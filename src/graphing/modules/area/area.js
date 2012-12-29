2f7eb4e0022aa00d7fc7e2eae940363ba4bda377
Eric Gaumer <egaumer@gmail.com>
2012-12-29 11:14:11 -0500

    add Google Analytics style area charts for time series

--- src/graphing/modules/area/area.js
+++ src/graphing/modules/area/area.js
@@ -1,138 +1,131 @@
 "use strict";
 
 angular.module('evo.graphing')
-    .directive('evoArea', function() {
+    .directive('evoArea', [function() {
         return {
 			restrict: 'E',
             /**
              * Sets up an isolate scope so that we don't inherent from parent.
              */
             scope: {
-                //fontSize: '=',
-                //onClick: '=',
-                //data: '='
+                fontSize: '=',
+                onClick: '=',
+                width: '=',
+                height: '=',
+                data: '='
             },
 			link: function(scope, element, attrs) {
 
-				// "_type" : "date_histogram"
+				var margin = {top: 20, right: 20, bottom: 30, left: 50};
+                var width = scope.width || 960;
+                var height = scope.height || 500;
+                var color = attrs.color || '#e5f3f9';
+                var fontColor = attrs.fontColor || '#000';
+                var fontSize = scope.fontSize || 14;
+                var format = d3.time.format('%m/%d');
+                var interpolate = attrs.interpolate || 'false';
 
-				var data = [{
-        			"time" : 1341100800000,
-        			"count" : 9
-      			}, {
-        			"time" : 1343779200000,
-        			"count" : 32
-      			}, {
-        			"time" : 1346457600000,
-        			"count" : 78
-      			}, {
-        			"time" : 1349049600000,
-        			"count" : 45
-      			}, {
-        			"time" : 1351728000000,
-        			"count" : 134
-      			}];
-
-      			//var data=[{time: 1341100800000, count : [10,100]},{time: 1343779200000, count: [20, 150]}, {time: 1346457600000, count: [30, 120]}];
-      			var xx = [
-      				[{time: 1341100800000, count:9},{time: 1343779200000, count:32}],
-      				[{time: 1343779200000, count:32},{time: 1346457600000, count:78}],
-      				[{time: 1346457600000, count:78},{time: 1349049600000, count:45}],
-      				[{time: 1349049600000, count:45},{time: 1351728000000, count:134}]
-      			];
-
-				var margin = {
-					top: 20,
-					right: 20,
-					bottom: 30,
-					left: 50
-				}
-				var width = 960 - margin.left - margin.right;
-				var height = 500 - margin.top - margin.bottom;
-
-				//var parseDate = d3.time.format('%d-%b-%y').parse;
+				width = width - margin.left - margin.right;
+				height = height - margin.top - margin.bottom;
 
 				var x = d3.time.scale()
-					//.domain();
-					//.domain([1341100800000, 1343779200000, 1346457600000, 1349049600000, 1351728000000]);
 					.range([0, width]);
-				//var x = d3.scale()
 
 				var y = d3.scale.linear()
 					.range([height, 0]);
 
 				var xAxis = d3.svg.axis()
 					.scale(x)
-					//.scale(function(d) {return scale(d.time); })
 					.orient('bottom');
 
 				var yAxis = d3.svg.axis()
 					.scale(y)
 					.orient('left');
 
-var line = d3.svg.line()
-	.interpolate('cardinal')
-    .x(function(d) { return x(d.time); })
-    .y(function(d) { return y(d.count); });
+                var line = d3.svg.line()
+	                //.interpolate('cardinal')
+                    .x(function(d) { return x(d.time); })
+                    .y(function(d) { return y(d.count); });
 
 				var area = d3.svg.area()
-					.interpolate('cardinal')
+					//.interpolate('cardinal')
 					.x(function(d) { return x(d.time); })
 					.y0(height)
 					.y1(function(d) { return y(d.count); });
 
+                if (attrs.interpolate == 'true') {
+                    line.interpolate('cardinal');
+                    area.interpolate('cardinal');
+                }
+
 				var svg = d3.select(element[0])
 					.append('svg')
-						.attr('width', width + margin.left + margin.right)
-						.attr('height', height + margin.top + margin.bottom)
+                        .attr('preserveAspectRatio', 'xMinYMin')
+                        .attr('viewBox', '0 0 ' + (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))
+						//.attr('width', width + margin.left + margin.right)
+						//.attr('height', height + margin.top + margin.bottom)
 						.append('g')
 							.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-				x.domain(d3.extent(data, function(d) { return d.time; }));
-				y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
-		for (var i=0; i < xx.length; i++) {
-				svg.append('path')
-					//.datum(data)
-					.datum(xx[i])
-					//.attr('class', 'area')
-					.attr('fill', 'steelBlue')
-					.attr('d', area);
-		}
+                scope.$watch('data', function(data) {
 
-				svg.append('g')
-					.attr('class', 'x axis')
-					.attr('transform', 'translate(0,' + height + ')')
-					.call(xAxis);
+                    if (data) {
+                        data = data.entries || [];
 
-				svg.append('g')
-					.attr('class', 'y axis')
-					.call(yAxis)
-					.append('text')
-						.attr('transform', 'rotate(-90)')
-						.attr('y', 6)
-						.attr('dy', '.71em')
-						.style('text-anchor', 'end')
-						.text('Price ($)');
+                        svg.selectAll('*').remove();
+				
+                        x.domain(d3.extent(data, function(d) { return d.time; }));
+				        y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
-	svg.append("path")
-		.datum(data)
-	    .attr("class", "line")
-	    .attr("d", line);
+				        svg.append('path')
+					        .datum(data)
+					        .attr('fill', color)
+					        .attr('d', area);
 
-	svg.selectAll(".dot")
-	    .data(data.filter(function(d) { return d.count; }))
-	  .enter().append("circle")
-	    .attr("class", "dot")
-	    .attr("cx", line.x())
-	    .attr("cy", line.y())
-	    .attr("r", 3.5);
+				        svg.append('g')
+                            .attr('fill', fontColor)
+                            .attr('font-size', fontSize)
+					        .attr('class', 'x axis')
+					        .attr('transform', 'translate(0,' + height + ')')
+					        .call(xAxis);
 
-				/*scope.$watch('data', function(data) {
+				        svg.append('g')
+					        .attr('class', 'y axis')
+                            .attr('font-size', fontSize)
+                            .attr('fill', fontColor)
+					        .call(yAxis)
+					        .append('text')
+						        .attr('transform', 'rotate(-90)')
+						        .attr('y', 6)
+						        .attr('dy', '.71em')
+						        .style('text-anchor', 'end')
+						        .text('Frequency');
 
-					data = data.terms || [];
-                    svg.selectAll('*').remove();
-				}*/
-			}
-        }
-    });
+                        /* draw the line on top of the area */
+	                    svg.append("path")
+		                    .datum(data)
+                            .attr('fill', 'none')
+                            .attr('stroke', '#058dc7')
+                            .attr('stroke-width', 3.5)
+	                        .attr("d", line);
+
+	                    svg.selectAll(".dot")
+	                        .data(data.filter(function(d) { return d.count; }))
+	                        .enter()
+                                .append("circle")
+                                    .attr('fill', '#058dc7')
+	                                .attr("cx", line.x())
+	                                .attr("cy", line.y())
+	                                .attr("r", 5.5)
+                                    .on('mousedown', function(d) { 
+                                        scope.$apply(function() {
+                                            (scope.onClick || angular.noop)(attrs.field, d.time);
+                                        });
+                                    });
+
+			        }
+                })
+            }
+        };
+    }]);
