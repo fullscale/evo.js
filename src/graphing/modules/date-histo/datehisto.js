0a9be719f7aeb03c582f8fd8f246a0005e7a0ba1
Eric Gaumer <egaumer@gmail.com>
2013-01-06 14:45:23 -0500

    refactor histogram chart

--- src/graphing/modules/date-histo/datehisto.js
+++ src/graphing/modules/date-histo/datehisto.js
@@ -1,51 +1,57 @@
-"use strict";
-
+/*
+ * Date Histogram 
+ * Copyright (c) 2012 FullScale Labs, LLC
+ */
 angular.module('evo.graphing')
     .directive('evoDateHisto', [function() {
+        'use strict';
+
         return {
-			restrict: 'E',
-            /**
-             * Sets up an isolate scope so that we don't inherent from parent.
-             */
+            // restrict this directive to being used as an element only
+			restrict: 'E', 
+
+            // sets up the isolate scope so that we don't clobber parent scope
             scope: {
-                fontSize: '=',
-                onClick: '=',
-                width: '=',
-                height: '=',
-                data: '='
+                // evaluated instances (bound once at link time)
+                onClick:   '=',
+                width:     '=',
+                height:    '=',
+                data:      '=',
+                // interpolated strings (bound to scope in watch fn)
+                label:     '@',
+                field:     '@',
+                duration:  '@'
             },
+
+            // angular directives return a link fn
 			link: function(scope, element, attrs) {
 
-                var margin = {top:20, right: 20, bottom: 30, left: 50};
-                var width = scope.width || 960;
-                var height = scope.height || 500;
-                var color = attrs.color || 'steelblue';
-                var fontColor = attrs.fontColor || '#000';
-                var fontSize = scope.fontSize || 14;
-                var format = d3.time.format("%m/%d");
-                var label = attrs.label || 'Frequency';
-
-                var ff = function(d) {
-                    var xx = new Date(d);
-                    if (xx.getDay()%7 === 0) {
-                        return format(xx);
-                    } else { return ''; }
+                var margin = {
+                    top:20, 
+                    right: 20, 
+                    bottom: 30, 
+                    left: 50
                 };
 
-                /* if no field param is set, use the facet name but normalize the case */
-                if (attrs.field == undefined) {
-                    attrs.field = attrs.data.split('.').pop().toLowerCase();
-                }
+                // default width/height - mainly to create aspect ratio since svg scales dynamically
+                var width = scope.width || 1280;
+                var height = scope.height || 300;
 
+                var label = attrs.label || 'Frequency';
+                var klass = attrs.class || '';
+
+                // add margin (make room for x,y labels)
                 width = width - margin.left - margin.right;
                 height = height - margin.top - margin.bottom;
 
+                // create x,y scales (x is inferred as time)
                 var x = d3.time.scale()
                     .range([0, width]);
 
                 var y = d3.scale.linear()
                     .range([height, 0]);
 
+                // create x,y axis
                 var xAxis = d3.svg.axis()
                     .scale(x)
                     .orient('bottom');
@@ -54,6 +60,7 @@ angular.module('evo.graphing')
                     .scale(y)
                     .orient('left');
 
+                // create the root svg node
                 var svg = d3.select(element[0])
                     .append('svg')
                         .attr('preserveAspectRatio', 'xMinYMin')
@@ -61,50 +68,90 @@ angular.module('evo.graphing')
                         .append('g')
                             .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+                // insert the x axis (no data yet)
+                svg.append('g')
+                    .attr('class', 'histo x axis ' + klass)
+                    .attr('transform', 'translate(0,' + height + ')')
+                    .call(xAxis);
 
+                // insert the y axis (no data yet)
+                svg.append('g')
+                    .attr('class', 'histo y axis ' + klass)
+                    .call(yAxis)
+                        .append('text')
+                            .attr('transform', 'rotate(-90)')
+                            .attr('y', 6)
+                            .attr('dy', '.51em')
+                            .style('text-anchor', 'end')
+                            .text(label);
+
+
+                // mainer observer fn called when scope is updated. Data and scope vars are npw bound
                 scope.$watch('data', function(data) {
 
+                    // pull info from scope
+                    var duration = scope.duration || 0;
+                    var delay = scope.delay || 0;
+                    var field = scope.field || attrs.data.split('.').pop().toLowerCase();
+
+                    // just because scope is bound doesn't imply we have data
                     if (data) {
+
+                        // pull the data array from the facet 
                         data = data.entries || [];
 
-                        svg.selectAll('*').remove();
+                        // calculate the bar width based on the data length leaving
+                        // a 2 pixel "gap" between bars.
+                        var barWidth = width/data.length - 2;
 
-                        x.domain(d3.extent(data, function(d) { return d.time; }));
+                        // recalculate the x and y domains based on the new data.
+                        // we have to add our "interval" to the max otherwise 
+                        // we don't have enough room to draw the last bar. Here it's
+                        // hardcoded to 1day (86400000 ms) but we need to somehow
+                        // "know" the interval the user used for the facet.
+                        x.domain([
+                            d3.min(data, function(d) { 
+                                return d.time;
+                            }), 
+                            d3.max(data, function(d) { 
+                                return d.time;
+                            }) + 86400000
+                        ]);
                         y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
-                        svg.append('g')
-                            .attr('fill', fontColor)
-                            .attr('font-size', fontSize)
-                            .attr('class', 'x axis')
-                            .attr('transform', 'translate(0,' + height + ')')
-                            .call(xAxis);
+                        // create transition (x,y axis)
+                        var t = svg.transition().duration(750);
 
-                        svg.append('g')
-                            .attr('class', 'y axis')
-                            .attr('font-size', fontSize)
-                            .attr('fill', fontColor)
-                            .call(yAxis)
-                            .append('text')
-                                .attr('transform', 'rotate(-90)')
-                                .attr('y', 6)
-                                .attr('dy', '.51em')
-                                .style('text-anchor', 'end')
-                                .text(label);
+                        // using a random key function here will cause all nodes to update
+                        var bars = svg.selectAll('rect')
+                            .data(data, function(d) { return Math.random(); });
 
-                        svg.selectAll('.bar')
-                            .data(data)
-                            .enter()
-                                .append('rect')
-                                    .attr('fill', color)
-                                    .attr('x', function(d) { return x(d.time); })
-                                    .attr('width', (width/data.length))
-                                    .attr('y', function(d) { return y(d.count); })
-                                    .attr('height', function(d) { return height - y(d.count); })
-                                    .on('mousedown', function(d) {
-                                        scope.$apply(function() {
-                                        (scope.onClick || angular.noop)(attrs.field, d.time);
-                                    });
-                                });
+                        // d3 enter fn binds each new value to a rect
+                        bars.enter()
+                            .append('rect')
+                                .attr('class', 'histo rect ' + klass)
+                                .attr('x', function(d) { return x(d.time); })
+                                .attr("y", function(d) { return height })
+                                .attr('width', barWidth)
+                                .transition()
+                                    .delay(function (d,i){ return i * delay; })
+                                    .duration(duration)
+                                        .attr('height', function(d) { return height - y(d.count); })
+                                        .attr('y', function(d) { return y(d.count); });
+
+                        // wire up event listeners - (registers filter callback)
+                        bars.on('mousedown', function(d) {
+                            scope.$apply(function() {
+                                (scope.onClick || angular.noop)(field, d.time);
+                            });
+                        });
+
+                        // d3 exit/remove flushes old values (removes old rects)
+                        bars.exit().remove();
+
+                        // update our x,y axis based on new data values
+                        t.select('.x').call(xAxis);
+                        t.select('.y').call(yAxis);
                     }
                 })
             }
